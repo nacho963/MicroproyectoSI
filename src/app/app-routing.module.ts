4fab83e963094fc217fdaa94984406242c80c031
nacho963 <46509023+nacho963@users.noreply.github.com>
2019-03-01 10:25:50 -0400

    InicioYHost

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { app } from 'firebase';
+import{AppComponent}from './app.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+];
 
 @NgModule({
+  declarations: [
+    AppComponent,
+  
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+const config = {
+  apiKey: "AIzaSyBCaG5k8PY2NL_8LjZIw11vxPunzgtLs54",
+authDomain: "microproyecto-6d50d.firebaseapp.com",
+databaseURL: "https://microproyecto-6d50d.firebaseio.com",
+projectId: "microproyecto-6d50d",
+storageBucket: "microproyecto-6d50d.appspot.com",
+messagingSenderId: "681952830694"
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
