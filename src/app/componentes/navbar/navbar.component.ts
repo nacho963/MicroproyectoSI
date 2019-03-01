20ec7580cb87b5a81995ad4b3dfb7617369b86e6
nacho963 <46509023+nacho963@users.noreply.github.com>
2019-03-01 17:36:48 -0400

    barranav

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -10,6 +10,18 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
+
+  openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+ closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
 
 }
